23e745a2e86ff16a21998606c886f5a86bd6d8bd
ngovanuc <ngovanuc.1508@gmail.com>
2025-05-19 21:08:00 +0700

    update app and llm

--- app.py
+++ app.py
@@ -3,22 +3,24 @@ from typing import Dict
 
 import chainlit as cl
 
-from src.starter.init_starter import init_starter
+from src.starters.set_starters import set_starters
 from src.chat_settings.on_settings_update import update_settings
 from src.chat_life_cycle.on_chat_start import on_chat_start
 from src.chat_life_cycle.on_message.on_message import on_message
 from src.chat_life_cycle.on_audio_chunk import on_audio_chunk
+from src.chat_life_cycle.on_audio_end import on_audio_end
 from src.chat_life_cycle.on_stop import on_stop
 from src.chat_life_cycle.on_action import on_action
 from src.chat_life_cycle.on_chat_end import on_chat_end
 from src.chat_life_cycle.on_loggout import on_logout
 
 
-cl.on_chat_start(init_starter)
+cl.set_starters(set_starters)
 cl.on_settings_update(update_settings)
 cl.on_chat_start(on_chat_start)
 cl.on_message(on_message)
 cl.on_audio_chunk(on_audio_chunk)
+cl.on_audio_end(on_audio_end)
 cl.on_stop(on_stop)
 cl.action_callback(on_action)
 cl.on_chat_end(on_chat_end)
--- src/llms/gpt.py
+++ src/llms/gpt.py
@@ -1,5 +1,6 @@
 class gpt_4_32k:
     def __init__(self):
-        self.temperature = 0.7
         self.model_name = "gpt-4-32k"
+        self.api_key = "insert_your_api_key"
+        self.temperature = 0.7
         self.activate = False
