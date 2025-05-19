da19c1f5c92d02324426f88a5e86a72b81dc240e
ngovanuc <ngovanuc.1508@gmail.com>
2025-05-19 21:15:07 +0700

    removed api key

--- src/llms/cohere.py
+++ src/llms/cohere.py
@@ -1,7 +1,7 @@
 class command_a_03_2025:
     def __init__(self):
         self.model_name = "command-a-03-2025"
-        self.api_key = "3cWLxhrfwFWSNiTiTVkHRjCTdjjiMCoh8qh0DT1X"
+        self.api_key = "insert_your_api_key"
         self.max_tokens = 4096
         self.temperature = 0
         self.activate = True
