b98b41d120738a025adf40684c4005748fbf8c3e
ngovanuc <ngovanuc.1508@gmail.com>
2025-05-21 21:35:25 +0700

    removed files

--- src/llms/openai.py
+++
@@ -1,6 +0,0 @@
-class gpt_4_32k:
-    def __init__(self):
-        self.model_name = "gpt-4-32k"
-        self.api_key = "openai_api_key"
-        self.temperature = 0.5
-        self.activate = False
