aeaccdf7ebc27b5968c8a0ed5d9dfd3b48cbe0da
ngovanuc <ngovanuc.1508@gmail.com>
2025-05-21 21:26:46 +0700

    upload llm providers

--- src/llms/anthropic.py
+++ src/llms/anthropic.py
@@ -2,7 +2,7 @@
 class claude_3_7_sonnet_20250219:
     def __init__(self):
         self.model_name = "claude-3-7-sonnet-20250219"
-        self.api_key = "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"
+        self.api_key = "insert_api_key"
         self.max_token = 1000
         self.temperature = 0.5
         self.activate = True
