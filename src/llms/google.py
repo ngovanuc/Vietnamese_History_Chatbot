7ce4cba7a9eb8dab0b6ad7564f86f3ffa9c121a9
ngovanuc <ngovanuc.1508@gmail.com>
2025-05-21 21:28:38 +0700

    add files

--- src/llms/google.py
+++ src/llms/google.py
@@ -1,6 +1,6 @@
 class gemini_20_flash:
     def __init__(self):
         self.model_name = "gemini-2.0-flash"
-        self.api_key = "AIzaSyBR-hBGb0yTH-XKbkQ9BCIQxNW6rUp9JAE"
+        self.api_key = "insert_api_key"
         self.temperature = 0.5
         self.activate = True
--- src/llms/groq.py
+++ src/llms/groq.py
@@ -1,7 +1,7 @@
 class llama3_70b_8192:
     def __init__(self):
         self.model_name = "llama3-70b-8192"
-        self.api_key = "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"
+        self.api_key = "insert_api_key"
         self.max_tokens = 8192
         self.temperature = 0.5
         self.activate = True
