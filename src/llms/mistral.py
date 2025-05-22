2225fad8bc56afe64f55972f4f89d4eb3b4ea6fb
ngovanuc <ngovanuc.1508@gmail.com>
2025-05-22 09:49:20 +0700

    add new llm provider

---
+++ src/llms/mistral.py
@@ -0,0 +1,6 @@
+class mistral_large_latest:
+    def __init__(self):
+        self.model_name = "mistral-large-latest"
+        self.api_key = "insert_api_key"
+        self.temperature = 0.5
+        self.activate = True
