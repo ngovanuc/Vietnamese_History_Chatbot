2e54625c70e6281647ca0a9e2c699d0c717a9b62
ngovanuc <ngovanuc.1508@gmail.com>
2025-06-05 14:39:35 +0700

    update va bo sung mot vai thu linh tinh khong quan trong

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
-from typing import Dict
-from typing import Dict
+import os
+os.system("cls")
 
 import chainlit as cl
 
-from src.chat_life_cycle.password_auth_callback import password_auth_callback
+from src.authentication.password_auth_callback import password_auth_callback
 from src.starters.set_starters import set_starters
 from src.chat_settings.on_settings_update import update_settings
 from src.chat_life_cycle.on_chat_start import on_chat_start
+from src.chat_life_cycle.realtime_chat.on_audio_start import on_audio_start
 from src.chat_life_cycle.on_chat_resume import on_chat_resume
 from src.chat_life_cycle.on_message.on_message import on_message
-from src.chat_life_cycle.on_audio_chunk import on_audio_chunk
-from src.chat_life_cycle.on_audio_end import on_audio_end
+from src.chat_life_cycle.realtime_chat.on_audio_chunk import on_audio_chunk
+from src.chat_life_cycle.realtime_chat.on_audio_end import on_audio_end
 from src.chat_life_cycle.on_stop import on_stop
 from src.chat_life_cycle.on_action import on_action
 from src.chat_life_cycle.on_chat_end import on_chat_end
@@ -21,6 +22,7 @@ cl.password_auth_callback(password_auth_callback)
 # cl.set_starters(set_starters)
 cl.on_settings_update(update_settings)
 cl.on_chat_start(on_chat_start)
+cl.on_audio_start(on_audio_start)
 cl.on_chat_resume(on_chat_resume)
 cl.on_message(on_message)
 cl.on_audio_chunk(on_audio_chunk)
