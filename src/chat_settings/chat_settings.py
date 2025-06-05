1cd9fc58873b44232c43466372cd9a82b7a41671
ngovanuc <ngovanuc.1508@gmail.com>
2025-06-05 14:40:50 +0700

    update for chat_settings

--- src/chat_settings/chat_settings.py
+++ src/chat_settings/chat_settings.py
@@ -31,4 +31,48 @@ chat_settings = [
         step=0.1,
         description="Giá trị cao cho phản hồi phong phú hơn"
     ),
+
+    Slider(
+        id="Silence_threshold",
+        label="Ngưỡng âm lượng.",
+        initial=3500,
+        min=1000,
+        max=6000,
+        step=100,
+        description="Ngưỡng thấp cho phép thu âm thanh nhỏ hơn."
+    ),
+
+    Slider(
+        id="Silence_timeout",
+        label="Thời gian đợi.",
+        initial=1500.0,
+        min=1000,
+        max=3000,
+        step=100,
+        description="Thời gian chờ đợi im lặng để kết thúc câu hỏi."
+    ),
+
+    Select(
+        id="Voice",
+        label="Chọn giọng nói",
+        values=[
+            "An",
+            "Tâm",
+            "Jessica",
+            "Louis",
+            "Alex",
+        ],
+        initial_index=0,
+        description="Chọn giọng nói yêu thích."
+    ),
+
+    Slider(
+        id="Voice_speed",
+        label="Tốc độ phản hồi âm thanh",
+        initial=180,
+        min=100,
+        max=300,
+        step=10,
+        description="Điều chỉnh tốc độ nói của chatbot."
+    ),
 ]
