f80a43e9d12c905cb1bf6def4111b635b8bd6b37
trunglevie1903 <trunglevie0319@gmail.com>
2025-06-13 19:41:19 +0700

    update file slide

--- src/llms/cohere.py
+++ src/llms/cohere.py
@@ -1,7 +1,7 @@
 class command_a_03_2025:
     def __init__(self):
         self.model_name = "command-a-03-2025"
-        self.api_key = "insert_api_key"
+        self.api_key = "hIQagEKA7L7ZUk7eKpfKYLof8rigsGTBoNvr8nX0"
         self.max_tokens = 4096
         self.temperature = 0.5
         self.activate = True
